d0ca7c6e5ca979e08bb0bfd05338c7e9a149ba00
Zoltan Benedek <zoltan@complyadvantage.com>
2016-03-28 16:20:24 +0300

    Improve the script for corner cases and remove some hardcoded stuff

--- correct_empty_lines.py
+++ correct_empty_lines.py
@@ -1,45 +1,40 @@
-print('\n\n\n\n')
-print('Now:\n')
+g.es('\n\n\n\n')
+g.es('Now:\n')
 
 
 for p in c.all_positions():
     try:
         # Corrects empty lines around @language python\n@tabwidth -4
         if p.h.startswith('@clean') and p.h.endswith('py'):
-            # Corrects empty lines after @first
-            if p.h == '@clean manage.py':
-                splited = p.b.partition('"""\nMain starter script for Django\n"""')
-                p.b = ''.join((splited[0].rstrip(), '\n', splited[1], splited[2]))
-
             try:
                 if '@language python\n@tabwidth -4' in p.b:
                     splited = p.b.partition('@language python\n@tabwidth -4')
                     p.b = ''.join((splited[0].rstrip(), '\n', splited[1], '\n', splited[2].lstrip()))
-                    print(p.h)
+                    g.es(p.h)
 
-            except Exception as exc:
+            except Exception:
                 g.es_exception()
 
         # Corrects empty lines between main blocks
         else:
-            parent = p.parent().h
-            if parent.startswith('@clean'):
+            parent = p.parent()
+            if parent.h.startswith('@clean'):
                 siblings = [pos.gnx for pos in p.self_and_siblings()]
                 # Corrects empty lines between main blocks
-                if p.gnx != siblings[-1]:
+                if p.gnx != siblings[-1] or not parent.b.rstrip().endswith('@others'):
                     p.b = '{}\n\n\n'.format(p.b.rstrip())
                 # Corrects empty lines at end of file
                 else:
                     p.b = '{}\n'.format(p.b.rstrip())
             # Corrects empty lines between methods
-            elif parent.startswith('class'):
+            elif parent.h.startswith('class'):
                 siblings = [pos.gnx for pos in p.self_and_siblings()]
                 # Corrects empty lines between main blocks
-                if p.gnx != siblings[-1]:
+                if p.gnx != siblings[-1] or not parent.b.rstrip().endswith('@others'):
                     p.b = '{}\n\n'.format(p.b.rstrip())
                 # Corrects empty lines at end of file
                 else:
                     p.b = '{}\n'.format(p.b.rstrip())
 
-    except Exception as exc:
+    except Exception:
         g.es_exception()
